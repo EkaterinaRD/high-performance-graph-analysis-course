6a7e37237ef3d453660415c94c4dd2790c0ee065
EkaterinaRD <katja-ribibna@yandex.ru>
2022-06-20 17:03:55 +0300

    fixed trc

--- project/trc.py
+++ project/trc.py
@@ -9,21 +9,19 @@ def trc(matrix: Matrix, start_v: int):
     if matrix.type != BOOL:
         raise ValueError("Mtx's type is not BOOL")
     if not matrix.iseq(matrix.transpose()):
-        raise ValueError("Graph is not undirected") 
+        matrix = matrix | matrix.transpose() 
    
+ 
+    tmp_mtx = matrix.mxm(matrix, cast=INT64, mask=matrix)
     
-    #matrix2 = matrix @ matrix
-    matrix2 = matrix.mxm(matrix, cast=INT64)
-    matrix2 = matrix2.emult(matrix, cast=INT64)
     
     n = matrix.nrows
+    
     result = list()
-    for j in range(n):
-        j_row = matrix2.extract_row(j)
-        elm_result = 0
-        for i in range(n):
-            elm_result += j_row.get(i, 0)
-        result.append(elm_result)
+    for i in range(n):
+        tmp = list()
+        tmp = tmp_mtx.extract_row(i).to_lists()[1]
+        result.append(sum(tmp) // 2)
     
     return result
     
