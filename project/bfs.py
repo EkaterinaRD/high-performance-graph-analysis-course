957ab81bb4f1408a229e633d75a912c2659a4b18
EkaterinaRD <katja-ribibna@yandex.ru>
2022-06-20 16:21:01 +0300

    change multi_bfs

--- project/bfs.py
+++ project/bfs.py
@@ -36,8 +36,39 @@ def bfs (matrix: Matrix, start_v: int):
     
 def multi_bfs(matrix: Matrix, start_vs: List[int]):
     
+    if not matrix.square:
+        raise ValueError("This matrix is not square")
+    n = matrix.nrows
+    m = len(start_vs)
+    for i in range(m)
+        if start_vs[i] >= n or start_v[i] < 0:
+            raise ValueError("Vertex is incorrect")
+         
+    
+    
+    front_mtx = Matrix.sparse(BOOL, nrows = m, ncols = n)
+    visited_mtx = Matrix.sparse(BOOL, nrows = m, ncols = n)
+    dist_mtx = Matrix.sparse(INT64, nrows = m, ncols = n)
+    
+    for r, v in enumerate(start_vs):
+        front_mtx.assign_scalar(True, r, v)
+        visited_mtx.assign_scalar(True, r, v)
+        dist_mtx.assign_scalar(0, r, v)
+    
+    level = 1
+    while level < n:
+        front_mtx = front_mtx.mxm(matrix, mask=visited_mtx, desc=RC)
+        visited_mtx = visited_mtx | front_mtx
+        dist_mtx.assign_scalar(level, mask=front_mtx)
+        level += 1
+    
     result = list()
-    for start_v in start_vs:
-        result.append((start_v, bfs(matrix,start_v)))
-        
+    
+    
+    for i, v in enumerate(start_vs):
+        res_row = list()
+        for j in range(n):
+            res_row.append(dist_mtx.get(i, j, -1))
+        result.append((v, res_row))
+    
     return result
