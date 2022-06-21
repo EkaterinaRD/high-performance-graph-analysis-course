0d5b65d3ff13a68523634760b8cd65af1bb78e8c
EkaterinaRD <katja-ribibna@yandex.ru>
2022-06-21 10:06:29 +0300

    fixed sssp

--- project/sssp.py
+++ project/sssp.py
@@ -3,39 +3,14 @@ from pygraphblas.types import INT64
 from typing import List
 
 
-def sssp(matrix: Matrix, start_v: int):
-    
-    if not matrix.square:
-        raise ValueError("This matrix is not square")
-    n = matrix.nrows
-    if start_v >= n or start_v < 0:
-        raise ValueError("Vertex is incorrect")
-        
-        
-    front = Vector.sparse(INT64, n)
-    front[start_v] = 0;
-   
- 
-    step = 0
-    while step < n:
-        front.vxm(matrix, semiring=INT64.min_plus, out=front, accum=INT64.min)
-        step += 1
-        
-    result = list()
-    for i in range(n):
-        result.append(front.get(i, -1))
-
-    return result
-   
-    
-def multi_sssp(matrix: Matrix, start_vs: List[int]):
+def _sssp(matrix: Matrix, start_vs: List[int]):
     
     if not matrix.square:
         raise ValueError("This matrix is not square")
     n = matrix.nrows
     m = len(start_vs)
-    for i in range(m)
-        if start_vs[i] >= n or start_v[i] < 0:
+    for i in range(m):
+        if start_vs[i] >= n or start_vs[i] < 0:
             raise ValueError("Vertex is incorrect")
     
     front_mtx = Matrix.sparse(INT64, nrows = m, ncols = n)
@@ -43,8 +18,11 @@ def multi_sssp(matrix: Matrix, start_vs: List[int]):
         front_mtx.assign_scalar(0, i, v)
     
     step = 0
-    while step < n:
+    is_change = True
+    while step < n and is_change:
+        prev = front_mtx
         front_mtx.mxm(matrix, semiring=INT64.min_plus, out=front_mtx, accum=INT64.min)
+        is_change = not prev.iseq(front_mtx)
         step += 1
     
     result = list()
@@ -57,3 +35,15 @@ def multi_sssp(matrix: Matrix, start_vs: List[int]):
         result.append((v, res_row))
     
     return result
+
+def sssp(matrix: Matrix, start_v: int):
+    
+    result = list()
+    result = _sssp(matrix, [start_v])[0][1]
+    
+    return result
+   
+    
+def multi_sssp(matrix: Matrix, start_vs: List[int]):
+    
+    return _sssp(matrix, start_vs)
